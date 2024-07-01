ec0e92dd7f53d96fbf4a743fe29cb2c040673b03
Badmatrix <matrix.og11@gmail.com>
2024-07-01 17:36:49 +0100

    upadte-- setting dynamic data attribute

--- index.js
+++ index.js
@@ -16,15 +16,19 @@ const now = new Date();
 const today = now.getUTCDay();
 const dayUtc = daysOfTheWeek[today];
 UtcDay.textContent = `${dayUtc}, `;
+UtcDay.setAttribute("data-testid", dayUtc)
 
 const hour = now.getHours();
 const minutes = now.getUTCMinutes();
 utcTime.textContent = `${hour}:${minutes}`;
 
 const date = now.getUTCDate()
+UtcDate.setAttribute("data-testid", date)
 const month = now.getUTCMonth();
 const year = now.getUTCFullYear();
 UtcDate.textContent = `${String(date).padStart(2, 0)}/${String(
   month + 1
 ).padStart(2, 0)}/${year}`;
 
+// console.log(UtcDate.getAttribute("data-testid"))
+// console.log(UtcDay.getAttribute("data-testid"))
